4c907de6b7d25d05d402d6f624a0faf09d3159fa
Alex Rocha <rocha.amc@gmail.com>
2023-04-10 15:44:54 -0700

    node reddit clone project init

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const { engine } = require('express-handlebars');
+
+const app = express();
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views')
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.listen(3000, () => {
+  console.log("Server started on http://localhost:3000")
+});
